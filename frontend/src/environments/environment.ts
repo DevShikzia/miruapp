509c1629d94af7a317fc2b83728822535d6b818e
DevShikzia <devsanchezmatias@gmail.com>
2026-06-25 18:12:37 -0300

    chore: restore placeholder googleClientId in environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
-  socketUrl: 'http://localhost:3000',
-  googleClientId: '381208022179-2sqreidgbhl3v2gdnglck0fs82mpnd8b.apps.googleusercontent.com',
+  apiUrl: '/api',
+  socketUrl: '',
+  googleClientId: '123456789-xxxxx.apps.googleusercontent.com',
 }
